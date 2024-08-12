902bc3e85d6351afb8865d50d51f2de41f637c7e
Harshvardhan Sharma <harsh.vsharma00@gmail.com>
2024-08-12 19:44:59 +0200

    A simple hangman game

---
+++ hangman.py
@@ -0,0 +1,105 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "camel"]
+lives = 6
+chosen_word = random.choice(word_list)
+print(chosen_word)
+blank = ""
+
+
+
+for position in range(len(chosen_word)):
+    blank += "_"
+print(blank)
+game_over = False
+correct_word = []
+
+while not game_over:
+    user_input = input("Guess a letter: ").lower()
+    
+    if user_input in correct_word:
+        print(f"You have already guessed '{user_input}' letter, guess another!")
+    
+    display = ""
+        
+    for letter in chosen_word:
+        if letter == user_input:
+            display += letter
+            correct_word.append(user_input)
+        elif letter in correct_word:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+    
+    
+    if user_input not in chosen_word:
+        lives -= 1
+        print(f"You have {lives} lives left")
+        if lives == 0:
+            game_over = True
+            print(f"It was {chosen_word}! You lose")
+            
+    if "_" not in display:
+        game_over = True
+        print("You Win!")
+        
+    print(stages[lives])
+
+
